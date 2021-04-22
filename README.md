12e56979a5b535eb72a44c50f9d3d7e90411d652
yagorms <yagoramos@hotmail.com>
2021-04-22 15:28:06 -0300

    Cursos Concluidos

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Cursos-SpringBoot-Alura
 Conjunto de 3 cursos de Spring Boot
+
+Spring Boot API REST: Construa uma API
+Spring Boot API Rest: Segurança da API, Cache e Monitoramento
+Spring Boot e Teste: Profiles, Testes e Deploy
---
+++ forum/src/main/java/br/com/alura/forum/config/validacao/ErroDeFormularioDTO.java
@@ -0,0 +1,22 @@
+package br.com.alura.forum.config.validacao;
+
+public class ErroDeFormularioDTO {
+
+	private String campo;
+	private String erro;
+
+	public ErroDeFormularioDTO(String campo, String erro) {
+		super();
+		this.campo = campo;
+		this.erro = erro;
+	}
+
+	public String getCampo() {
+		return campo;
+	}
+
+	public String getErro() {
+		return erro;
+	}
+
+}
